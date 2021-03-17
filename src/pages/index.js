257480784eea1b779ad8ed71aacd93c03ed62859
nazufel <38038720+nazufel@users.noreply.github.com>
2021-03-16 20:27:01 -0400

    finished call to actions on the index page. thinking about putting them on the images

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,54 +14,60 @@ const IndexPage = () => (
     <SEO title="Home" />
 
     <div className="parallax-wire-edm" />
-    <div style={{  marginTop: 20, marginBottom: 20, textAlign: "center" }}>
-    <Typography variant="h3" color="primary">
-      Leaders in Precision Wire EDM
-    </Typography>
-    <Typography variant="h2" style={{ margin: 20 }}>
-      Put Our Four Generations of Experience on Your Next Project
-    </Typography>
-    <div>
-      <Button 
-          color="primary"
-          href="/about"
-          variant="contained" 
-        >
-          About Us
-        </Button>
-    </div>
+      <div style={{  marginTop: 20, marginBottom: 20, textAlign: "center" }}>
+      <Typography variant="h3" color="primary">
+        Leaders in Precision Wire EDM
+      </Typography>
+      <Typography variant="h2" style={{ margin: 20 }}>
+        Put Our Four Generations of Experience on Your Next Project
+      </Typography>
+      <div>
+        <Button 
+            color="primary"
+            href="/about"
+            variant="contained" 
+          >
+            About Us
+          </Button>
+      </div>
     </div>
 
     <div className="parallax-cnc" />
     <div style={{  marginTop: 20, marginBottom: 20, textAlign: "center" }}>
-    <Typography variant="h3" color="primary">
-      We Use the Latest Technology in Tool and Die Manufacturing
-    </Typography>
-    <Typography variant="h2" style={{ margin: 20 }}>
-      Learn About Our Advanced Equipment for Your Next Project
-    </Typography>
-    <div>
-      <Button 
-          color="primary"
-          href="/equipment"
-          variant="contained" 
-        >
-          Equipment
-        </Button>
-    </div>
+      <Typography variant="h3" color="primary">
+        We Use the Latest Technology in Tool and Die Manufacturing
+      </Typography>
+      <Typography variant="h2" style={{ margin: 20 }}>
+        Learn About Our Advanced Equipment for Your Next Project
+      </Typography>
+      <div>
+        <Button 
+            color="primary"
+            href="/equipment"
+            variant="contained" 
+          >
+            Equipment
+          </Button>
+      </div>
     </div>
     <div className="parallax-surface-grinding" />
-    <CallToAction
-      actionText="Our Technology Is Ready for Your Next Project"
-      actionLink="/equipment"
-      buttonText="Equipment"
-    />
-    <div className="parallax-milling" />
-    <CallToAction
-      actionText="Ready to Get Started?"
-      actionLink="/contact"
-      buttonText="Contact Us"
-    />
+    <div style={{  marginTop: 20, marginBottom: 20, textAlign: "center" }}>
+      <Typography variant="h3" color="primary">
+         Ready to Get Started?
+      </Typography>
+      <Typography variant="h2" style={{ margin: 20 }}>
+        Contact Us Today
+      </Typography>
+      <div>
+        <Button 
+            color="primary"
+            href="/contact"
+            variant="contained" 
+          >
+            Contact
+          </Button>
+      </div>
+    </div>
   </FrontPageLayout>
 )
 
