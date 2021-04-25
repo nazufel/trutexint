89be95584d52a694454ea4d194e81228dd9718e1
nazufel <38038720+nazufel@users.noreply.github.com>
2021-04-25 01:45:19 +0000

    have chevron pointing down

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import Button from '@material-ui/core/Button';
 import { Typography } from "@material-ui/core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import FrontPageLayout from "../components/frontPageLayout";
 import SEO from "../components/seo";
 
@@ -11,25 +13,37 @@ const IndexPage = () => (
   <FrontPageLayout>
     <SEO title="Home" />
 
-    <div class="container" style={{ textAlign: "center" }}>
-      <div className="parallax-wire-edm" style={{  marginTop: 50, marginBottom: 20 }}/>
-        <Typography variant="h3" color="primary">
-          Leaders in Precision Wire EDM
-        </Typography>
-        <Typography variant="h2" style={{ margin: 20 }}>
-          Put Our Four Generations of Experience on Your Next Project
-        </Typography>
-          <Button 
-              color="primary"
-              href="/about"
-              variant="contained" 
-            >
-              About Us
-            </Button>
+    <div style={{  marginTop: 500, marginBottom: 300, textAlign: "center" }}>
+      <Typography variant="h3" color="primary">
+        TRU-TEX International Corporation
+      </Typography>
+      <Typography variant="h2" style={{ margin: 20 }}>
+        The Mark of Quality
+      </Typography>
     </div>
 
+    <div style={{ textAlign: "center" }}>
+      <FontAwesomeIcon icon={ faChevronDown } style={{ marginBottom: 100, fontSize: 100}}/>
+    </div>
 
-
+    <div class="container" style={{ textAlign: "center" }}>
+    <div className="parallax-wire-edm" style={{  marginTop: 50, marginBottom: 20 }}/>
+      <Typography variant="h3" color="primary">
+        Leaders in Precision Wire EDM
+      </Typography>
+      <Typography variant="h2" style={{ margin: 20 }}>
+        Put Our Four Generations of Experience on Your Next Project
+      </Typography>
+      <div>
+        <Button 
+            color="primary"
+            href="/about"
+            variant="contained" 
+          >
+            About Us
+          </Button>
+      </div>
+    </div>
 
     <div className="parallax-cnc" />
     <div style={{  marginTop: 20, marginBottom: 20, textAlign: "center" }}>
@@ -49,6 +63,7 @@ const IndexPage = () => (
           </Button>
       </div>
     </div>
+
     <div className="parallax-surface-grinding" />
     <div style={{  marginTop: 20, marginBottom: 20, textAlign: "center" }}>
       <Typography variant="h3" color="primary">
